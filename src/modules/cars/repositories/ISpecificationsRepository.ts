d96947b6a6a9f1cbbfe41986739dd9d3ef014289
neanderdev <neander.faria@hotmail.com>
2022-06-22 10:31:32 -0300

    Creating specification repository

--- src/modules/cars/repositories/ISpecificationsRepository.ts
+++ src/modules/cars/repositories/ISpecificationsRepository.ts
@@ -1,1 +1,13 @@
-ISpecificationsRepository
+import { Specification } from "../model/Specification";
+
+interface ICreateSpecificationDTO {
+  name: string;
+  description: string;
+}
+
+interface ISpecificationsRepository {
+  create({ name, description }: ICreateSpecificationDTO);
+  findByName(name: string): Specification;
+}
+
+export { ISpecificationsRepository, ICreateSpecificationDTO };
