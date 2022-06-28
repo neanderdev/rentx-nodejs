6f00414f91a8dc6340a3a818055ce34d6d572e79
neanderdev <neander.faria@hotmail.com>
2022-06-28 14:15:39 -0300

    Continuing the cars use case

--- src/modules/cars/infra/typeorm/repositories/CarsRepository.ts
+++ src/modules/cars/infra/typeorm/repositories/CarsRepository.ts
@@ -1,8 +1,14 @@
 import { ICreateCarDTO } from "@modules/cars/dtos/ICreateCarDTO";
 import { ICarsRepository } from "@modules/cars/repositories/ICarsRepository";
 
+import { Car } from "../entities/Car";
+
 class CarsRepository implements ICarsRepository {
-  async create(data: ICreateCarDTO): Promise<void> {
+  async create(data: ICreateCarDTO): Promise<Car> {
+    throw new Error("Method not implemented.");
+  }
+
+  async findByLicensePlate(license_plate: string): Promise<Car> {
     throw new Error("Method not implemented.");
   }
 }
